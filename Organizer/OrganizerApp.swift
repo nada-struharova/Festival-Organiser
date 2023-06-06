98354441873ba5e7b4f6c695d94d7858bc985201
Nada Struharova <nada.struharova@icloud.com>
2023-06-06 12:25:28 +0100

    Initial Commit

---
+++ Organizer/OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizerApp.swift
+//  Organizer
+//
+//  Created by Nada Struharova on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
